dcb29cb5be9babd97506dc769a9f2c4989bc5894
Blake Coverett <blake@bcdev.com>
2023-01-23 13:43:48 -0800

    gallery generation update

--- tools/New-Gallery.ps1
+++ tools/New-Gallery.ps1
@@ -1,10 +1,11 @@
-Get-ChildItem images | ForEach-Object {
+param([string]$name, [string]$title)
+$body = Get-ChildItem "$name/images/*" | ForEach-Object {
     if ($_.Name -match '^(\d+)-(\d+)x(\d+)\.jpg$') {
         $i = $_.Name
         $n = $matches[1]
         $w = $matches[2]
         $h = $matches[3]
-        $t = @(Get-ChildItem "thumbs/$n-*.jpg")
+        $t = @(Get-ChildItem "$name/thumbs/$n-*.jpg")
         if ($t.Count -eq 1) {
             $t = $t[0].Name
             Write-Output "<a href='images/$i' data-pswp-width='$w' data-pswp-height='$h' data-cropped='true' target='_blank'><img src='thumbs/$t'></a>"
@@ -15,3 +16,22 @@ Get-ChildItem images | ForEach-Object {
         Write-Warning "Bad image name: $_"
     }
 }
+"<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Jake Lundrigan Memorial &mdash; $title</title>
+    <link rel='stylesheet' href='/photoswipe/photoswipe.css'>
+    <link rel='stylesheet' href='/style.css'>
+    <script type='module' src='/script.js'></script>
+</head>
+<body>
+    <h1><a href='/'>«&nbsp;</a>$title Gallery</h1>
+    <div id='gallery' class='gallery'>
+$body
+$((1..20|ForEach-Object {"<div class='filler'></div>"}) -join "`r`n")
+    </div>
+</body>
+</html>
+" | Out-File -Encoding utf8 "$name/index.html"
